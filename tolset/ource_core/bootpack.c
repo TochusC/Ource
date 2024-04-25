503db3ca01d81f26c28bf93c308a3a9dc7e3d5a8
TochusC <tochusc@gmail.com>
2024-04-25 16:12:00 +0800

    Update OURCE OS:
    Allow keyboard and mouse interrupt.
    
    Add GUI Interface.
    
    (08days)

--- tolset/ource_core/bootpack.c
+++ tolset/ource_core/bootpack.c
@@ -1,337 +1,189 @@
-void io_hlt(void);
-void io_cli(void);
-void io_out8(int port, int data);
-int io_load_eflags(void);
-void io_store_eflags(int eflags);
+/* bootpack�̃��C�� */
 
-void init_palette(void);
-void set_palette(int start, int end, unsigned char *rgb);
-void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+#include "bootpack.h"
+#include <stdio.h>
 
-void draw_char(unsigned char *vram, int xsize, unsigned char c, int color, int x, int y, int font_size);
-void draw_word(unsigned char *vram, char *word, int xsize, unsigned char color, int x, int y, int font_size);
+struct MOUSE_DEC {
+	unsigned char buf[3], phase;
+	int x, y, btn;
+};
 
-#define COL8_000000		0
-#define COL8_FF0000		1
-#define COL8_00FF00		2
-#define COL8_FFFF00		3
-#define COL8_0000FF		4
-#define COL8_FF00FF		5
-#define COL8_00FFFF		6
-#define COL8_FFFFFF		7
-#define COL8_C6C6C6		8
-#define COL8_840000		9
-#define COL8_008400		10
-#define COL8_848400		11
-#define COL8_000084		12
-#define COL8_840084		13
-#define COL8_008484		14
-#define COL8_848484		15
+extern struct FIFO8 keyfifo, mousefifo;
+void enable_mouse(struct MOUSE_DEC *mdec);
+void init_keyboard(void);
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
 
 void HariMain(void)
 {
-	char *p; /* p�Ƃ����ϐ��́ABYTE [...]�p�̔Ԓn */
+	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
+	char s[40], mcursor[256], keybuf[32], mousebuf[128];
+	int mx, my, i;
+	struct MOUSE_DEC mdec;
 
-	init_palette(); /* �p���b�g��ݒ� */
+	init_gdtidt();
+	init_pic();
+	io_sti(); /* IDT/PIC�̏��������I������̂�CPU�̊��荞�݋֎~������ */
+	fifo8_init(&keyfifo, 32, keybuf);
+	fifo8_init(&mousefifo, 128, mousebuf);
+	io_out8(PIC0_IMR, 0xf9); /* PIC1�ƃL�[�{�[�h������(11111001) */
+	io_out8(PIC1_IMR, 0xef); /* �}�E�X������(11101111) */
 
-	p = (char *) 0xa0000; /* �Ԓn���� */
+	init_keyboard();
 
-	draw_word(p, "WELCOME TO OURSE", 320, COL8_FF0000,  20,  20, 3);
-    draw_word(p,  "WELCOME TO OURSE", 320, COL8_00FF00,  120,  60, 2);
-    draw_word(p,  "WELCOME TO OURSE", 320, COL8_0000FF, 10,  90, 2);
+	init_palette();
+	init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
+	mx = (binfo->scrnx - 16) / 2; /* ��ʒ����ɂȂ�悤�ɍ��W�v�Z */
+	my = (binfo->scrny - 28 - 16) / 2;
+	init_mouse_cursor8(mcursor, COL8_008484);
+	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+	sprintf(s, "(%3d, %3d)", mx, my);
+	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+
+    putfonts8_asc(binfo->vram, binfo->scrnx, 48, 48, COL8_000000, "Welcome to OURCE OS!");
+    putfonts8_asc(binfo->vram, binfo->scrnx, 49, 49, COL8_FFFFFF, "Welcome to OURCE OS!");
+
+    putfonts8_asc(binfo->vram, binfo->scrnx, 120, 120, COL8_000000, "Powered by TochusC(UPC)");
+    putfonts8_asc(binfo->vram, binfo->scrnx, 120, 120, COL8_FFFFFF, "Powered by TochusC(UPC)");
+
+
+	enable_mouse(&mdec);
 
 	for (;;) {
-		io_hlt();
+		io_cli();
+		if (fifo8_status(&keyfifo) + fifo8_status(&mousefifo) == 0) {
+			io_stihlt();
+		} else {
+			if (fifo8_status(&keyfifo) != 0) {
+				i = fifo8_get(&keyfifo);
+				io_sti();
+				sprintf(s, "%02X", i);
+				boxfill8(binfo->vram, binfo->scrnx, COL8_008484,  0, 16, 15, 31);
+				putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+			} else if (fifo8_status(&mousefifo) != 0) {
+				i = fifo8_get(&mousefifo);
+				io_sti();
+				if (mouse_decode(&mdec, i) != 0) {
+					/* �f�[�^��3�o�C�g�������̂ŕ\�� */
+					sprintf(s, "[lcr %4d %4d]", mdec.x, mdec.y);
+					if ((mdec.btn & 0x01) != 0) {
+						s[1] = 'L';
+					}
+					if ((mdec.btn & 0x02) != 0) {
+						s[3] = 'R';
+					}
+					if ((mdec.btn & 0x04) != 0) {
+						s[2] = 'C';
+					}
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 15 * 8 - 1, 31);
+					putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+					/* �}�E�X�J�[�\���̈ړ� */
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, mx, my, mx + 15, my + 15); /* �}�E�X���� */
+					mx += mdec.x;
+					my += mdec.y;
+					if (mx < 0) {
+						mx = 0;
+					}
+					if (my < 0) {
+						my = 0;
+					}
+					if (mx > binfo->scrnx - 16) {
+						mx = binfo->scrnx - 16;
+					}
+					if (my > binfo->scrny - 16) {
+						my = binfo->scrny - 16;
+					}
+					sprintf(s, "(%3d, %3d)", mx, my);
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 0, 79, 15); /* ���W���� */
+					putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s); /* ���W���� */
+					putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16); /* �}�E�X�`�� */
+				}
+			}
+		}
 	}
 }
 
-void init_palette(void)
-{
-	static unsigned char table_rgb[16 * 3] = {
-		0x00, 0x00, 0x00,	/*  0:�� */
-		0xff, 0x00, 0x00,	/*  1:���邢�� */
-		0x00, 0xff, 0x00,	/*  2:���邢�� */
-		0xff, 0xff, 0x00,	/*  3:���邢���F */
-		0x00, 0x00, 0xff,	/*  4:���邢�� */
-		0xff, 0x00, 0xff,	/*  5:���邢�� */
-		0x00, 0xff, 0xff,	/*  6:���邢���F */
-		0xff, 0xff, 0xff,	/*  7:�� */
-		0xc6, 0xc6, 0xc6,	/*  8:���邢�D�F */
-		0x84, 0x00, 0x00,	/*  9:�Â��� */
-		0x00, 0x84, 0x00,	/* 10:�Â��� */
-		0x84, 0x84, 0x00,	/* 11:�Â����F */
-		0x00, 0x00, 0x84,	/* 12:�Â��� */
-		0x84, 0x00, 0x84,	/* 13:�Â��� */
-		0x00, 0x84, 0x84,	/* 14:�Â����F */
-		0x84, 0x84, 0x84	/* 15:�Â��D�F */
-	};
-	set_palette(0, 15, table_rgb);
-	return;
+#define PORT_KEYDAT				0x0060
+#define PORT_KEYSTA				0x0064
+#define PORT_KEYCMD				0x0064
+#define KEYSTA_SEND_NOTREADY	0x02
+#define KEYCMD_WRITE_MODE		0x60
+#define KBC_MODE				0x47
 
-	/* static char ���߂́A�f�[�^�ɂ����g���Ȃ�����DB���ߑ��� */
-}
-
-void set_palette(int start, int end, unsigned char *rgb)
+void wait_KBC_sendready(void)
 {
-	int i, eflags;
-	eflags = io_load_eflags();	/* ���荞�݋��t���O�̒l���L�^���� */
-	io_cli(); 					/* ���t���O��0�ɂ��Ċ��荞�݋֎~�ɂ��� */
-	io_out8(0x03c8, start);
-	for (i = start; i <= end; i++) {
-		io_out8(0x03c9, rgb[0] / 4);
-		io_out8(0x03c9, rgb[1] / 4);
-		io_out8(0x03c9, rgb[2] / 4);
-		rgb += 3;
-	}
-	io_store_eflags(eflags);	/* ���荞�݋��t���O�����ɖ߂� */
-	return;
-}
-
-void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1)
-{
-	int x, y;
-	for (y = y0; y <= y1; y++) {
-		for (x = x0; x <= x1; x++)
-			vram[y * xsize + x] = c;
+	/* �L�[�{�[�h�R���g���[�����f�[�^���M�\�ɂȂ�̂�҂� */
+	for (;;) {
+		if ((io_in8(PORT_KEYSTA) & KEYSTA_SEND_NOTREADY) == 0) {
+			break;
+		}
 	}
 	return;
 }
 
-const unsigned char font_data_array[26][5][7] = {
-        // A
-        {
-                {0, 1, 1, 0, 0},
-                {1, 0, 0, 1, 0},
-                {1, 1, 1, 1, 0},
-                {1, 0, 0, 1, 0},
-                {1, 0, 0, 1, 0}
-        },
-        // B
-        {
-                {1, 1, 1, 0, 0},
-                {1, 0, 0, 1, 0},
-                {1, 1, 1, 0, 0},
-                {1, 0, 0, 1, 0},
-                {1, 1, 1, 0, 0}
-        },
-        // C
-        {
-                {0, 1, 1, 0, 0},
-                {1, 0, 0, 1, 0},
-                {1, 0, 0, 0, 0},
-                {1, 0, 0, 1, 0},
-                {0, 1, 1, 0, 0}
-        },
-        // D
-        {
-                {1, 1, 0, 0, 0},
-                {1, 0, 1, 0, 0},
-                {1, 0, 0, 1, 0},
-                {1, 0, 1, 0, 0},
-                {1, 1, 0, 0, 0}
-        },
-        // E
-        {
-                {1, 1, 1, 0, 0},
-                {1, 0, 0, 0, 0},
-                {1, 1, 1, 0, 0},
-                {1, 0, 0, 0, 0},
-                {1, 1, 1, 0, 0}
-        },
-        // F
-        {
-                {1, 1, 1, 0, 0},
-                {1, 0, 0, 0, 0},
-                {1, 1, 1, 0, 0},
-                {1, 0, 0, 0, 0},
-                {1, 0, 0, 0, 0}
-        },
-        // G
-        {
-                {0, 1, 1, 0, 0},
-                {1, 0, 0, 0, 0},
-                {1, 0, 1, 1, 0},
-                {1, 0, 0, 1, 0},
-                {0, 1, 1, 0, 0}
-        },
-        // H
-        {
-                {1, 0, 0, 1, 0},
-                {1, 0, 0, 1, 0},
-                {1, 1, 1, 1, 0},
-                {1, 0, 0, 1, 0},
-                {1, 0, 0, 1, 0}
-        },
-        // I
-        {
-                {0, 1, 1, 0, 0},
-                {0, 0, 1, 0, 0},
-                {0, 0, 1, 0, 0},
-                {0, 0, 1, 0, 0},
-                {0, 1, 1, 0, 0}
-        },
-        // J
-        {
-                {0, 0, 1, 1, 0},
-                {0, 0, 0, 1, 0},
-                {0, 0, 0, 1, 0},
-                {1, 0, 0, 1, 0},
-                {0, 1, 1, 0, 0}
-        },
-        // K
-        {
-                {1, 0, 0, 1, 0},
-                {1, 0, 1, 0, 0},
-                {1, 1, 0, 0, 0},
-                {1, 0, 1, 0, 0},
-                {1, 0, 0, 1, 0}
-        },
-        // L
-        {
-                {1, 0, 0, 0, 0},
-                {1, 0, 0, 0, 0},
-                {1, 0, 0, 0, 0},
-                {1, 0, 0, 0, 0},
-                {1, 1, 1, 0, 0}
-        },
-        // M
-        {
-                {1, 0, 0, 0, 1},
-                {1, 1, 0, 1, 1},
-                {1, 0, 1, 0, 1},
-                {1, 0, 0, 0, 1},
-                {1, 0, 0, 0, 1}
-        },
-        // N
-        {
-                {1, 0, 0, 1, 0},
-                {1, 1, 0, 1, 0},
-                {1, 0, 1, 1, 0},
-                {1, 0, 0, 1, 0},
-                {1, 0, 0, 1, 0}
-        },
-        // O
-        {
-                {0, 1, 1, 0, 0},
-                {1, 0, 0, 1, 0},
-                {1, 0, 0, 1, 0},
-                {1, 0, 0, 1, 0},
-                {0, 1, 1, 0, 0}
-        },
-        // P
-        {
-                {1, 1, 1, 0, 0},
-                {1, 0, 0, 1, 0},
-                {1, 1, 1, 0, 0},
-                {1, 0, 0, 0, 0},
-                {1, 0, 0, 0, 0}
-        },
-        // Q
-        {
-                {0, 1, 1, 0, 0},
-                {1, 0, 0, 1, 0},
-                {1, 0, 1, 1, 0},
-                {1, 0, 0, 1, 0},
-                {0, 1, 1, 1, 0}
-        },
-        // R
-        {
-                {1, 1, 1, 0, 0},
-                {1, 0, 0, 1, 0},
-                {1, 1, 1, 0, 0},
-                {1, 0, 1, 0, 0},
-                {1, 0, 0, 1, 0}
-        },
-        // S
-        {
-                {0, 1, 1, 1, 0},
-                {1, 0, 0, 0, 0},
-                {0, 1, 1, 0, 0},
-                {0, 0, 0, 1, 0},
-                {1, 1, 1, 0, 0}
-        },
-        // T
-        {
-                {1, 1, 1, 1, 0},
-                {0, 0, 1, 0, 0},
-                {0, 0, 1, 0, 0},
-                {0, 0, 1, 0, 0},
-                {0, 0, 1, 0, 0}
-        },
-        // U
-        {
-                {1, 0, 0, 1, 0},
-                {1, 0, 0, 1, 0},
-                {1, 0, 0, 1, 0},
-                {1, 0, 0, 1, 0},
-                {0, 1, 1, 0, 0}
-        },
-        // V
-        {
-                {1, 0, 0, 1, 0},
-                {1, 0, 0, 1, 0},
-                {1, 0, 0, 1, 0},
-                {0, 1, 0, 1, 0},
-                {0, 0, 1, 0, 0}
-        },
-        // W
-        {
-                {1, 0, 0, 0, 1},
-                {1, 0, 1, 0, 1},
-                {1, 0, 1, 0, 1},
-                {0, 1, 0, 1, 0},
-                {0, 1, 0, 1, 0}
-        },
-        // X
-        {
-                {1, 0, 0, 1, 0},
-                {1, 0, 1, 0, 0},
-                {0, 1, 0, 1, 0},
-                {1, 0, 1, 0, 0},
-                {1, 0, 0, 1, 0}
-        },
-        // Y
-        {
-                {1, 0, 0, 1, 0},
-                {1, 0, 0, 1, 0},
-                {0, 1, 1, 0, 0},
-                {0, 0, 1, 0, 0},
-                {0, 0, 1, 0, 0}
-        },
-        // Z
-        {
-                {1, 1, 1, 1, 0},
-                {0, 0, 1, 0, 0},
-                {0, 1, 0, 0, 0},
-                {1, 0, 0, 1, 0},
-                {1, 1, 1, 1, 0}
-        }
-};
-
-
-void draw_char(unsigned char *vram, int xsize, unsigned char c, int color, int x, int y, int font_size) {
-    int char_index = c - 'A';
-
-    char *font_data = font_data_array[char_index];
-
-    int i, j;
-    for (j = 0; j < 7; j++) {
-        for ( i= 0; i < 5; i++) {
-            if (font_data[i * 7 + j] == 1) {
-                boxfill8(vram, xsize, color, x + j * font_size, y + i * font_size, x + j * font_size + font_size, y + i * font_size + font_size);
-            }
-        }
-    }
+void init_keyboard(void)
+{
+	/* �L�[�{�[�h�R���g���[���̏����� */
+	wait_KBC_sendready();
+	io_out8(PORT_KEYCMD, KEYCMD_WRITE_MODE);
+	wait_KBC_sendready();
+	io_out8(PORT_KEYDAT, KBC_MODE);
+	return;
 }
 
-void draw_word(unsigned char *vram, char *word, int xsize, unsigned char color, int x, int y, int font_size) {
-    int i = 0;
-    while (word[i] != '\0') {
-        if (word[i] == ' ') {
-            i++;
-            continue;
-        }
-        draw_char(vram, xsize, word[i], color, x + i * 6 * font_size, y, font_size);
-        i++;
-    }
+#define KEYCMD_SENDTO_MOUSE		0xd4
+#define MOUSECMD_ENABLE			0xf4
+
+void enable_mouse(struct MOUSE_DEC *mdec)
+{
+	/* �}�E�X�L�� */
+	wait_KBC_sendready();
+	io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
+	wait_KBC_sendready();
+	io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
+	/* ���܂�������ACK(0xfa)�����M����Ă��� */
+	mdec->phase = 0; /* �}�E�X��0xfa��҂��Ă���i�K */
+	return;
+}
+
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat)
+{
+	if (mdec->phase == 0) {
+		/* �}�E�X��0xfa��҂��Ă���i�K */
+		if (dat == 0xfa) {
+			mdec->phase = 1;
+		}
+		return 0;
+	}
+	if (mdec->phase == 1) {
+		/* �}�E�X��1�o�C�g�ڂ�҂��Ă���i�K */
+		if ((dat & 0xc8) == 0x08) {
+			/* ������1�o�C�g�ڂ����� */
+			mdec->buf[0] = dat;
+			mdec->phase = 2;
+		}
+		return 0;
+	}
+	if (mdec->phase == 2) {
+		/* �}�E�X��2�o�C�g�ڂ�҂��Ă���i�K */
+		mdec->buf[1] = dat;
+		mdec->phase = 3;
+		return 0;
+	}
+	if (mdec->phase == 3) {
+		/* �}�E�X��3�o�C�g�ڂ�҂��Ă���i�K */
+		mdec->buf[2] = dat;
+		mdec->phase = 1;
+		mdec->btn = mdec->buf[0] & 0x07;
+		mdec->x = mdec->buf[1];
+		mdec->y = mdec->buf[2];
+		if ((mdec->buf[0] & 0x10) != 0) {
+			mdec->x |= 0xffffff00;
+		}
+		if ((mdec->buf[0] & 0x20) != 0) {
+			mdec->y |= 0xffffff00;
+		}
+		mdec->y = - mdec->y; /* �}�E�X�ł�y�����̕�������ʂƔ��� */
+		return 1;
+	}
+	return -1; /* �����ɗ��邱�Ƃ͂Ȃ��͂� */
 }
